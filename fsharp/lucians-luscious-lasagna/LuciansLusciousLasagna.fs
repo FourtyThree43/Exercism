2971a518c51a8fb15c4d2eb01204752f8268f0f7
FourtyThree43 <shaqmwa@outlook.com>
2024-04-28 12:27:36 +0300

    F# -> Lucians Luscious Lasagna

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,15 @@
+module LuciansLusciousLasagna
+
+// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
+
+// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven elapsedMinutes =
+    expectedMinutesInOven - elapsedMinutes
+
+// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes layers = layers * 2
+
+// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes elapsed layers =
+    preparationTimeInMinutes elapsed + layers
